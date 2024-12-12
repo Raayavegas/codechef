d5d5501637e977f6256d87bf912341f850efd720
Veerala Rithika <veeralarithika@gmail.com>
2024-12-12 14:28:26 +0530

    Add files via upload

---
+++ area.java
@@ -0,0 +1,11 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+	    // Update the blanks in the code below
+		double pi = 3.14;
+		double radius = 8.9;
+		double area = pi * radius * radius;
+		System.out.println("The Area of the given Circle is " + area);
+	}
+}
