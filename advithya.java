5427d5f4deb18b8156e7a5294c9feb2f6f03af7c
Veerala Rithika <veeralarithika@gmail.com>
2024-12-12 14:27:20 +0530

    Add files via upload

---
+++ advithya.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	    if(n>=1 && n<=18)
+	    {
+	    if(n>=16 && n<=18)
+	    {
+	        System.out.println("ADVITIYA");
+	    }
+	    else{
+	         System.out.println(" WAITING FOR ADVITIYA");
+	    }
+	    }
+	}
+}
